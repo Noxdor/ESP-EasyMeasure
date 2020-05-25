a6a63775c898ae47f153766dbc459318f712a7c8
Lennart Guderian | HERMOS SYSTEMS <lguderian@hermos.de>
2020-05-25 08:44:04 +0200

    Syntax error

--- HermosEasyMeasure.ino
+++ HermosEasyMeasure.ino
@@ -108,7 +108,7 @@ void setup()
 
   //Setup redirects
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    if (on==true) request->redirect("/main")
+    if (on==true) request->redirect("/main");
     request->send(SPIFFS, "/start.html", "text/html", false, processor);
     last_adress = request->url();
   });
